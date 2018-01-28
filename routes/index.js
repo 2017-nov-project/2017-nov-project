a003510c849833448465eaa78fce4d17fadce816
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-28 14:03:33 +0000

    Remove routes/index.js

--- routes/index.js
+++
@@ -1,6 +0,0 @@
-const apiRouter = require('./api');
-const postcodeRouter = require('./postcode');
-const housesRouter = require('./houses');
-const countyRouter = require('./county')
-
-module.exports = {apiRouter, postcodeRouter, housesRouter, countyRouter};
