5999b6c2ca38e4c13fb3e730945c7c10110692f4
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-30 17:14:48 +0000

    Update postcodes production collection

--- seed/seed.production.js
+++ seed/seed.production.js
@@ -4,13 +4,19 @@ const { exec } = require('child_process');
 require('dotenv').config({ path: `./.env.production` });
 const { SOURCE, USERNAME, PASSWORD } = process.env;
 
+
+
 fs.readdir(path.join(__dirname, './currentData/housesData'), (err, files) => {
   if (err) throw err;
-  files.map(file => {
-    console.log(`file: ${file}`)
-    exec(`mongoimport -h ${SOURCE} -d housing-heatmap -c houses --type csv --file ./seed/currentData/housesData/${file} --fields gov_unique_id,pricepaid,deed_date,postcode,property_type,new_build,estate_type,paon,saon,street,locality,town,district,county,transaction_category,record_status -u ${USERNAME} -p ${PASSWORD}`, err => {
-      if (err) throw err;
-      console.log(`File ${file} content added to db!`)
+  exec(`mongoimport -h ${SOURCE} -d housing-heatmap -c postcodes --type csv --file ./seed/currentData/ukpostcodes.csv --headerline -u ${USERNAME} -p ${PASSWORD}`, err => {
+    if (err) throw err;
+    console.log('Postcodes data added!');
+    files.map(file => {
+      console.log(`file: ${file}`)
+      exec(`mongoimport -h ${SOURCE} -d housing-heatmap -c houses --type csv --file ./seed/currentData/housesData/${file} --fields gov_unique_id,pricepaid,deed_date,postcode,property_type,new_build,estate_type,paon,saon,street,locality,town,district,county,transaction_category,record_status -u ${USERNAME} -p ${PASSWORD}`, err => {
+        if (err) throw err;
+        console.log(`File ${file} content added to db!`);
+      })
     })
-  })
+  });
 })
