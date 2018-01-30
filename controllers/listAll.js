28dee8f6f0a02495e0dce67c78a958ece6c705be
Jac <jacdarby@gmail.com>
2018-01-30 15:47:14 +0000

    refactors

--- controllers/listAll.js
+++ controllers/listAll.js
@@ -5,22 +5,12 @@ const listAll = (req, res, next) => {
 
 let [[key, value]] = Object.entries(req.params)
 
-console.log(key, value);
-
-
-
 if (key === 'town') key = 'city';
-
-console.log({key})
 
 const collection = {
   city: Town,
   locality: Locality
 }
-
-console.log(Object.keys(collection))
-
-console.log('collection accessed', collection[key]);
 
   collection[key].find({[key]: new RegExp('^' + value)}).lean()
     .then(results => results.map(result => result[key]).slice(0,10))
--- seed/seed.js
+++ seed/seed.js
@@ -12,14 +12,14 @@ const seedDB = dbUrl => {
     const houses = housesData.map(house => _.omit(house, '_id'));
 
     const localities = localityData.map(locality => ({'locality':locality}))
-    //  const counties = countyData.map(county => ({'county':county}))
+    const counties = countyData.map(county => ({'county':county}))
     return mongoose.connect(dbUrl)
         .then(() => mongoose.connection.db.dropDatabase())
         .then(() => House.collection.insertMany(houses))
         .then(() => Postcode.collection.insertMany(postcodeData))
         .then(() => Town.collection.insertMany(townsData))
         .then(() => Locality.collection.insertMany(localities))
-        //  .then(() => County.collection.insertMany(counties))
+        .then(() => County.collection.insertMany(counties))
         .then(console.log('Records added, closing connection.'))
         .then(() => mongoose.disconnect())
         .catch(console.log)
