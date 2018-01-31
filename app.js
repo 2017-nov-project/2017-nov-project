88bd64bd245309ae6143a044f7200e9b66b06b45
dzewelina <ewelina.zukowska26@gmail.com>
2018-01-31 09:26:26 +0000

    Clear dependencies

--- app.js
+++ app.js
@@ -7,15 +7,12 @@ const cors = require('cors')
 const mongoose = require('mongoose');
 if (process.env.NODE_ENV !== 'production') require('dotenv').config({ path: `./.env.${process.env.NODE_ENV}` });
 const { DB_URL } = process.env
-app.engine('md', require('marked-engine').renderFile);
 
 mongoose.Promise = Promise;
 mongoose.connect(DB_URL);
 mongoose.connection.on('error', console.error.bind(console, 'MongoDB connection error:'))
 
 app.use(cors());
-app.use(bodyparser.json());
-
 
 app.use('/api', apiRouter);
 
